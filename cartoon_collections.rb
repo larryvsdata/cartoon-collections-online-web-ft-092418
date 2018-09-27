3110450a340a43ac2c79e405430919d6d872cd33
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-27 23:58:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,10 @@ def summon_captain_planet(array)
   array.map{|item| item.capitalize<<"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |my_str|
+  my_str.length>4? true: false 
+end
 end
 
 def find_the_cheese# code an argument here
